86337e5d7995f80a4e798203c89304bc6e4aa100
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-25 19:58:25 +0530

    jsx

--- ev-website/src/App.jsx
+++ ev-website/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Background from './components/Background/Background';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
+
+  let heroData = [
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give in to", text2:"your passions"},
+  ]
+  
+  const[heroCount, setHeroCount] = useState(2);
+  const[playStatus, setPlayStatus] = useState(false);
+
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={setHeroCount}/>
+      <Navbar/>
     </div>
   )
 }
